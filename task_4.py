77885a86c2b1b7bc778725956acdf5458df1e945
Семён <karachinstar@yandex.ru>
2023-03-14 08:08:46 +0800

    Решение четвертой задачи

--- task_4.py
+++ task_4.py
@@ -1,2 +1,10 @@
 revenue = int(input('Введите выручку фирмы: '))
 costs = int(input('Введите издержки фирмы: '))
+if revenue > costs:
+    print(f'Финансовый результат - прибыль. Ее величина: {revenue - costs}')
+    print(f'Рентабельность выручки = {costs / revenue}')
+    number_of_staff = int(input('Введите численность сотрудников: '))
+    print(f'Прибыль фирмы в расчете на одного сотрудника '
+          f'=  {(revenue - costs) / number_of_staff}')
+else:
+    print(f'Финансовый результат - убыток. Его величина: {costs - revenue}')
